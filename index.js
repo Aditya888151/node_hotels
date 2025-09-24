715bbf0ceb1a58272566990ed360356c27ce6c3b
Aditya888151 <singhaditya8052@gmail.com>
2025-09-24 17:02:15 +0530

    testing

--- index.js
+++ index.js
@@ -1,17 +1,10 @@
+require('dotenv').config();
 const express = require('express');
-const mongoose = require('mongoose');
 const cors = require('cors');
 const path = require('path');
+const db = require('./db');
 
 const app = express();
-
-// MongoDB connection
-const mongoURL = 'mongodb+srv://singhaditya8052_db_user:Aditya8892@cluster0.h42azqe.mongodb.net/restaurant';
-mongoose.connect(mongoURL);
-
-// Models
-const Person = require('./models/person');
-const Menu = require('./models/menu');
 
 // Middleware
 app.use(cors());
@@ -19,88 +12,22 @@ app.use(express.json());
 app.use(express.static('public'));
 
 // Routes
+const personRoutes = require('./routes/personRoutes');
+const menuRoutes = require('./routes/menuRoutes');
+
+app.use('/person', personRoutes);
+app.use('/menu', menuRoutes);
+app.use('/api/person', personRoutes);
+app.use('/api/menu', menuRoutes);
+
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 app.get('/health', (req, res) => {
-  res.json({ status: 'OK', db: mongoose.connection.readyState });
+  res.json({ status: 'OK', timestamp: new Date() });
 });
 
-// Person routes
-app.get('/person', async (req, res) => {
-  try {
-    const people = await Person.find();
-    res.json(people);
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
-});
 
-app.post('/person', async (req, res) => {
-  try {
-    const person = new Person(req.body);
-    await person.save();
-    res.json(person);
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
-});
-
-app.put('/person/:id', async (req, res) => {
-  try {
-    const person = await Person.findByIdAndUpdate(req.params.id, req.body, { new: true });
-    res.json(person);
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
-});
-
-app.delete('/person/:id', async (req, res) => {
-  try {
-    await Person.findByIdAndDelete(req.params.id);
-    res.json({ message: 'Deleted' });
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
-});
-
-// Menu routes
-app.get('/menu', async (req, res) => {
-  try {
-    const menu = await Menu.find();
-    res.json(menu);
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
-});
-
-app.post('/menu', async (req, res) => {
-  try {
-    const menu = new Menu(req.body);
-    await menu.save();
-    res.json(menu);
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
-});
-
-app.put('/menu/:id', async (req, res) => {
-  try {
-    const menu = await Menu.findByIdAndUpdate(req.params.id, req.body, { new: true });
-    res.json(menu);
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
-});
-
-app.delete('/menu/:id', async (req, res) => {
-  try {
-    await Menu.findByIdAndDelete(req.params.id);
-    res.json({ message: 'Deleted' });
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
-});
 
 module.exports = app;
